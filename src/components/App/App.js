e78a1ac356702c8367a6b5e1c7628d9bc697daa3
Rod Glover <rglover@uvic.ca>
2017-10-18 14:05:12 -0700

    Place App in components/ folder

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <p>Insert awesomeness here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
