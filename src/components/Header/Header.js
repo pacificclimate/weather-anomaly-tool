2463cf9dcb8a8c48b998d71ac842116d2f357f55
Rod Glover <rglover@uvic.ca>
2017-10-18 14:07:54 -0700

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import classNames from 'classnames';
+
+import styles from './Header.css';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className={classNames(styles.header)}>
+        <Grid fluid>
+          <Row>
+            <Col lg={4}>
+              <a href='https://pacificclimate.org/'>
+                <img
+                  src={require('./logo.png')}
+                  width='328'
+                  height='38'
+                  alt='Pacific Climate Impacts Consortium'
+                />
+              </a>
+            </Col>
+            <Col lg={4} />
+            <Col lg={4} />
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Header;
