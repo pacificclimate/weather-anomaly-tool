a7a9670d915dc1cdbdffa08df36a508efee85d80
Rod Glover <rglover@uvic.ca>
2017-10-19 09:28:48 -0700

    Add initial ScrollingSelector

--- src/components/ScrollingSelector/ScrollingSelector.js
+++ src/components/ScrollingSelector/ScrollingSelector.js
@@ -6,28 +6,37 @@ import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 class ScrollingSelector extends Component {
     render() {
         const toggleButtons = this.props.options.map((option) => (
-            <ToggleButton value={option.value}>{option.label}</ToggleButton>
-        ))
+            <ToggleButton
+                className="ScrollingSelector-button"
+                key={option.value} value={option.value}
+            >
+                {option.label}
+            </ToggleButton>
+        ));
         return (
-            <div>
-                <ToggleButtonGroup
-                    vertical type="radio" name={this.props.name}
-                    defaultValue={this.props.defaultValue}
-                    onChange={this.props.onChange}
-                >
-                    {toggleButtons}
-                </ToggleButtonGroup>
-
-            </div>
+            <ToggleButtonGroup
+                className="ScrollingSelector"
+                style={{height: `${(this.props.height * 1.43).toString()}em`}}
+                vertical type="radio" name={this.props.name}
+                defaultValue={this.props.defaultValue}
+                onChange={this.props.onChange}
+            >
+                {toggleButtons}
+            </ToggleButtonGroup>
         )
     }
 }
 
 ScrollingSelector.propTypes = {
+    height: PropTypes.number,
     name: PropTypes.string.isRequired,
     options: PropTypes.array.isRequired,
     defaultValue: PropTypes.any,
     onChange: PropTypes.func.isRequired,
+};
+
+ScrollingSelector.defaultProps = {
+    height: 10,
 };
 
 export default ScrollingSelector;
