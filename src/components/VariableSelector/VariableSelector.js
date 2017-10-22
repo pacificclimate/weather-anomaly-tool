cf3d62a11a801a1206e5463538c9ac4cc32341ad
Rod Glover <rglover@change.org>
2017-10-22 12:31:16 -0700

    Refactor VariableSelector to wrap RadioButtonSelector

--- src/components/VariableSelector/VariableSelector.js
+++ src/components/VariableSelector/VariableSelector.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Tooltip } from 'react-bootstrap';
+import RadioButtonSelector from '../RadioButtonSelector';
 import './VariableSelector.css';
 
-import { ToggleButtonGroup, ToggleButton, Glyphicon, Tooltip } from 'react-bootstrap';
 
 class VariableSelector extends Component {
     render() {
+        const variables = [
+            { value: 'precip', label: 'Precipitation', },
+            { value: 'tmin', label: <span>T<sub>min</sub></span>, },
+            { value: 'tmax', label: <span>T<sub>max</sub></span>, },
+        ];
         return (
-            <div>
-                <ToggleButtonGroup
-                    vertical type="radio" name="variable"
-                    defaultValue={this.props.defaultValue}
-                    onChange={this.props.onChange}
-                >
-                    <ToggleButton value={'precip'}>
-                        <Glyphicon glyph="cloud"/> Precipitation
-                    </ToggleButton>
-                    <ToggleButton value={'tmin'}>
-                        <Glyphicon glyph="arrow-down"/> T<sub>min</sub>
-                    </ToggleButton>
-                    <ToggleButton value={'tmax'}>
-                        <Glyphicon glyph="arrow-up"/> T<sub>max</sub>
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </div>
+            <RadioButtonSelector name="variable" options={variables} onChange={this.props.onChange}/>
+            // <div>
+            //     <ToggleButtonGroup
+            //         vertical type="radio" name="variable"
+            //         defaultValue={this.props.defaultValue}
+            //         onChange={this.props.onChange}
+            //     >
+            //         <ToggleButton value={'precip'}>
+            //             <Glyphicon glyph="cloud"/> Precipitation
+            //         </ToggleButton>
+            //         <ToggleButton value={'tmin'}>
+            //             <Glyphicon glyph="arrow-down"/> T<sub>min</sub>
+            //         </ToggleButton>
+            //         <ToggleButton value={'tmax'}>
+            //             <Glyphicon glyph="arrow-up"/> T<sub>max</sub>
+            //         </ToggleButton>
+            //     </ToggleButtonGroup>
+            // </div>
         );
     }
 }
