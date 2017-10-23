4d62c841cb783938bb22966b505956c9ed21b0fe
Rod Glover <rglover@uvic.ca>
2017-10-23 11:47:14 -0700

    Create DataLoaderMode component

---
+++ src/components/DataLoaderMode/DataLoaderMode.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { pick } from '../utils';
+import RadioButtonSelector from '../RadioButtonSelector';
+
+import './DataLoaderMode.css';
+
+class DataLoaderMode extends Component {
+    render() {
+        const modes = [
+            {value: 'fake', label: 'Fake',},
+            {value: 'real', label: 'Real',},
+        ];
+        return (
+            <RadioButtonSelector
+                name="dataloader-mode"
+                options={modes}
+                {...pick(this.props, 'value onChange')}
+            />
+        );
+    }
+}
+
+DataLoaderMode.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+};
+
+export default DataLoaderMode;
