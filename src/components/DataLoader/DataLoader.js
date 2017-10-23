398d093f6ec893a3a5d5a7c93c9c0486f60400c0
Rod Glover <rglover@uvic.ca>
2017-10-23 10:49:27 -0700

    Load more dummy data

--- src/components/DataLoader/DataLoader.js
+++ src/components/DataLoader/DataLoader.js
@@ -3,63 +3,19 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import './DataLoader.css';
 
+import baseline_precip_1 from './dummy-data/baseline-precip-1.json';
+import baseline_precip_2 from './dummy-data/baseline-precip-2.json';
+import monthly_precip_1990_1 from './dummy-data/monthly-precip-1990-1.json';
+import monthly_precip_1990_2 from './dummy-data/monthly-precip-1990-2.json';
+
 const data = [
     {
-        baseline: [{
-            "network_name": "EC",
-            "datum": 120.0,
-            "history_db_id": 404,
-            "station_name": "ACTIVE PASS",
-            "elevation": 4.0,
-            "station_native_id": "1010066",
-            "lon": -123.283333,
-            "lat": 48.866667,
-            "station_db_id": 1
-        }],
-        monthly: [{
-            "network_name": "EC",
-            "history_db_id": 404,
-            "data_coverage": 0.741935483870968,
-            "station_name": "ACTIVE PASS",
-            "cell_method": "time: sum",
-            "network_variable_name": "ONE_DAY_PRECIPITATION",
-            "frequency": "daily",
-            "statistic": 118.6,
-            "lon": -123.283333,
-            "station_native_id": "1010066",
-            "elevation": 4.0,
-            "lat": 48.866667,
-            "station_db_id": 1
-        }],
+        baseline: baseline_precip_1,
+        monthly: monthly_precip_1990_1,
     },
-
     {
-        baseline: [{
-            "network_name": "EC",
-            "datum": 64.0,
-            "history_db_id": 1200,
-            "station_name": "PRINCE GEORGE MIWORTH",
-            "elevation": 610.0,
-            "station_native_id": "1096465",
-            "lon": -122.933333,
-            "lat": 53.966667,
-            "station_db_id": 797
-        }],
-        monthly: [{
-            "network_name": "EC",
-            "history_db_id": 1200,
-            "data_coverage": 0.870967741935484,
-            "station_name": "PRINCE GEORGE MIWORTH",
-            "cell_method": "time: sum",
-            "network_variable_name": "ONE_DAY_PRECIPITATION",
-            "frequency": "daily",
-            "statistic": 73.6,
-            "lon": -122.933333,
-            "station_native_id": "1096465",
-            "elevation": 610.0,
-            "lat": 53.966667,
-            "station_db_id": 797
-        }],
+        baseline: baseline_precip_2,
+        monthly: monthly_precip_1990_2,
     },
 ];
 let index = 0;
