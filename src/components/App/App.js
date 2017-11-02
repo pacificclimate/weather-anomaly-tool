d6964cfe8ae398533c448e4a41ba9d881cfcbb05
Rod Glover <rglover@uvic.ca>
2017-11-02 13:39:56 -0700

    Reformat code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+
 import Header from '../Header';
 import Tool from '../Tool'
 
+import './App.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Tool/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Tool/>
+            </div>
+        );
+    }
 }
 
 export default App;
