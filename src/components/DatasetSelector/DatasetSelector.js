f0be5ed0d8b42fb84cd481d43415c5f0fd3e7cea
Rod Glover <rglover@uvic.ca>
2017-11-09 10:20:49 -0800

    Make DatasetSelector a PureComponent (works)

--- src/components/DatasetSelector/DatasetSelector.js
+++ src/components/DatasetSelector/DatasetSelector.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import withLifeCycleLogging from '../../HOCs/withLifeCycleLogging';
@@ -7,13 +7,14 @@ import RadioButtonSelector from '../RadioButtonSelector';
 import './DatasetSelector.css';
 
 
-class DatasetSelector extends Component {
+const datasets = [
+    { value: 'anomaly', label: 'Anomaly', },
+    { value: 'monthly', label: 'Monthly', },
+    { value: 'baseline', label: 'Baseline', },
+];
+
+class DatasetSelector extends PureComponent {
     render() {
-        const datasets = [
-            { value: 'anomaly', label: 'Anomaly', },
-            { value: 'monthly', label: 'Monthly', },
-            { value: 'baseline', label: 'Baseline', },
-        ];
         return (
             <RadioButtonSelector
                 className={this.props.className}
