2e0b73b0667f094402394e6176814572d99b6158
Rod Glover <rglover@uvic.ca>
2017-11-14 10:33:11 -0800

    Fix name and header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,26 +8,21 @@ class Header extends Component {
 
     render() {
         return (
-            <div className={classNames(styles.header)}>
-                <Grid fluid>
-                    <Row>
-                        <Col lg={4}>
-                            <a href='https://pacificclimate.org/'>
-                                <img
-                                    src={require('./logo.png')}
-                                    width='328'
-                                    height='38'
-                                    alt='Pacific Climate Impacts Consortium'
-                                />
-                            </a>
-                        </Col>
-                        <Col lg={4}>
-                            <h1>Weather Anomaly Tool</h1>
-                        </Col>
-                        <Col lg={4}/>
-                    </Row>
-                </Grid>
-            </div>
+            <Row className={'Header'}>
+                <Col lg={3} className="text-left">
+                    <a href='https://pacificclimate.org/'>
+                        <img
+                            src={require('./logo.png')}
+                            width='328'
+                            height='38'
+                            alt='Pacific Climate Impacts Consortium'
+                        />
+                    </a>
+                </Col>
+                <Col>
+                    <h1>Weather Anomaly Viewer</h1>
+                </Col>
+            </Row>
         );
     }
 }
--- src/components/Tool/Tool.js
+++ src/components/Tool/Tool.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import logger from '../../logger';
 import { bindFunctions, pick } from '../utils';
@@ -66,78 +66,76 @@ class Tool extends PureComponent {
     render() {
         const isBaselineDataset = this.state.dataset === 'baseline';
         return (
-            <Grid fluid className="Tool">
-                <Row>
-                    <Col lg={3} className="selectors">
-                        <Row>Display</Row>
-                        <Row>
-                            <Col lg={2}/>
-                            <Col lg={4}>
-                                <VariableSelector
-                                    disabled={this.state.dataLoading}
-                                    value={this.state.variable}
-                                    onChange={this.handleChangeVariable}
-                                />
-                            </Col>
-                            <Col lg={4}>
-                                <DatasetSelector
-                                    value={this.state.dataset}
-                                    onChange={this.handleChangeDataset}
-                                />
-                            </Col>
-                            <Col lg={2}/>
-                        </Row>
-                        <Row>for</Row>
-                        <Row>
-                            <Col lg={8}>
-                                <MonthSelector
-                                    disabled={this.state.dataLoading}
-                                    value={this.state.month}
-                                    onChange={this.handleChangeMonth}
-                                />
-                            </Col>
-                            <Col lg={4}>
-                                <IncrementDecrement
-                                    disabled={this.state.dataLoading}
-                                    id="month-increment"
-                                    bsSize="xsmall"
-                                    by={[1, 3, 6]}
-                                    onIncrement={this.handleIncrementMonth}
-                                />
-                            </Col>
-                        </Row>
-                        {!isBaselineDataset &&
-                        <Row>
-                            <Col lg={8}>
-                                <YearSelector
-                                    disabled={this.state.dataLoading}
-                                    start={1970} end={2018}
-                                    value={this.state.year}
-                                    onChange={this.handleChangeYear}
-                                />
-                            </Col>
-                            <Col lg={4}>
-                                <IncrementDecrement
-                                    disabled={this.state.dataLoading}
-                                    id="year-increment"
-                                    bsSize="xsmall"
-                                    by={[1, 2, 3, 4, 5, 10]}
-                                    onIncrement={this.handleIncrementYear}
-                                />
-                            </Col>
-                        </Row>}
-                    </Col>
-                    <Col  lg={9}>
-                        <DataViewer
-                            {...pick(this.state, 'dataset variable year month')}
-                            onDataWillLoad={this.handleDataIsLoading}
-                            onDataDidLoad={this.handleDataIsNotLoading}
-                            onDataDidCatch={this.handleDataIsNotLoading}
-                        />
-                    </Col>
-                </Row>
-            </Grid>
-        )
+            <Row className="Tool">
+                <Col lg={3} className="selectors">
+                    <Row>Display</Row>
+                    <Row>
+                        <Col lg={2}/>
+                        <Col lg={4}>
+                            <VariableSelector
+                                disabled={this.state.dataLoading}
+                                value={this.state.variable}
+                                onChange={this.handleChangeVariable}
+                            />
+                        </Col>
+                        <Col lg={4}>
+                            <DatasetSelector
+                                value={this.state.dataset}
+                                onChange={this.handleChangeDataset}
+                            />
+                        </Col>
+                        <Col lg={2}/>
+                    </Row>
+                    <Row>for</Row>
+                    <Row>
+                        <Col lg={8}>
+                            <MonthSelector
+                                disabled={this.state.dataLoading}
+                                value={this.state.month}
+                                onChange={this.handleChangeMonth}
+                            />
+                        </Col>
+                        <Col lg={4}>
+                            <IncrementDecrement
+                                disabled={this.state.dataLoading}
+                                id="month-increment"
+                                bsSize="xsmall"
+                                by={[1, 3, 6]}
+                                onIncrement={this.handleIncrementMonth}
+                            />
+                        </Col>
+                    </Row>
+                    {!isBaselineDataset &&
+                    <Row>
+                        <Col lg={8}>
+                            <YearSelector
+                                disabled={this.state.dataLoading}
+                                start={1970} end={2018}
+                                value={this.state.year}
+                                onChange={this.handleChangeYear}
+                            />
+                        </Col>
+                        <Col lg={4}>
+                            <IncrementDecrement
+                                disabled={this.state.dataLoading}
+                                id="year-increment"
+                                bsSize="xsmall"
+                                by={[1, 2, 3, 4, 5, 10]}
+                                onIncrement={this.handleIncrementYear}
+                            />
+                        </Col>
+                    </Row>}
+                </Col>
+                <Col  lg={9}>
+                    <DataViewer
+                        {...pick(this.state, 'dataset variable year month')}
+                        onDataWillLoad={this.handleDataIsLoading}
+                        onDataDidLoad={this.handleDataIsNotLoading}
+                        onDataDidCatch={this.handleDataIsNotLoading}
+                    />
+                </Col>
+            </Row>
+        );
     }
 }
 
