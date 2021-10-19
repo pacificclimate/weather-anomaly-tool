de86cdd071c2e6d54ed057da34ac4ed5b6792302
rod-glover <rod.glover.ca@gmail.com>
2021-10-19 09:28:17 -0700

    Code cleanup

--- src/components/DataMap/DataMap.js
+++ src/components/DataMap/DataMap.js
@@ -1,9 +1,4 @@
 // DataMap: Component that displays a map with data.
-//
-// Currently, we're accepting data in the output format from the monthly Anomaly Data Service endpoints
-// /baseline and /monthly. That may be refactored as we progress.
-//
-// For prop definitions, see comments in BCMap.propTypes.
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
@@ -13,7 +8,7 @@ import { LayerGroup, LayersControl, CircleMarker } from 'react-leaflet';
 import _ from 'lodash';
 
 import { BCBaseMap } from 'pcic-react-leaflet-components';
-import { bindFunctions, pick } from '../utils';
+import { pick } from '../utils';
 import StaticControl from '../StaticControl';
 import StationPopup from '../StationPopup';
 import stationColor from './stationColor';
@@ -37,9 +32,11 @@ function uniqueKey(station) {
 }
 
 function StationLocationMarkers({stations}) {
-    // Return a set of markers (<CircleMarker/>) for the locations of each station in `props.station`.
-    // Icon markers `<Marker/>` don't work in this environment. I think it is because Webpack isn't including the
-    // image files that are needed. Certainly the GETs for those images fail. But circle markers work.
+    // Return a set of markers (<CircleMarker/>) for the locations of each
+    // station in `props.station`. Icon markers `<Marker/>` don't work in this
+    // environment. I think it is because Webpack isn't including the image
+    // files that are needed. Certainly the GETs for those images fail. But
+    // circle markers work.
     return stations.map(station =>
         <CircleMarker
             key={uniqueKey(station)}
@@ -50,8 +47,8 @@ function StationLocationMarkers({stations}) {
 }
 
 function StationDataMarkers({variable, dataset, stations}) {
-    // Return a set of markers (<CircleMarker/>) for the data for each station in `station`.
-
+    // Return a list of markers (<CircleMarker/>) for the data for each station
+    // in `station`.
     return stations.map(station =>
         <CircleMarker
             key={uniqueKey(station)}
@@ -65,38 +62,13 @@ function StationDataMarkers({variable, dataset, stations}) {
 }
 
 class DataMap extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            faderColor: '#777777',
-            faderOpacity: 0.6,
-        };
-
-        this.baselineMarkers = [];  // Necessary?
-
-        // Bind event handlers
-        bindFunctions(this, 'handleMapFaderChangeColor handleMapFaderChangeOpacity handleRefFaderLayer');
-    }
-
-    handleMapFaderChangeColor(faderColor) {
-        this.setState({faderColor}, () => this.faderLayer.redraw());
-    }
-
-    handleMapFaderChangeOpacity(faderOpacity) {
-        this.setState({faderOpacity});
-    }
-
-    handleRefFaderLayer(component) {
-        this.faderLayer = component.leafletElement;
-        this.faderLayer.createTile = () => {
-            const tile = document.createElement('div');
-            tile.style.background = this.state.faderColor;
-            // tile.style.background = '#000000';
-            // tile.style.background = '#ffffff';
-            return tile;
-        };
-        this.faderLayer.redraw();
-    }
+    static propTypes = {
+        variable: PropTypes.oneOf(["precip", "tmin", "tmax"]).isRequired,
+        dataset: PropTypes.oneOf(["anomaly", "monthly", "baseline"]).isRequired,
+        monthly: PropTypes.array.isRequired,
+        baseline: PropTypes.array.isRequired,
+        message: PropTypes.string,
+    };
 
     stationsForDataset() {
         // Return a set of stations determined by `this.props.dataset`.
@@ -104,15 +76,26 @@ class DataMap extends PureComponent {
         // For `anomaly`, compute the anomaly for stations for which there is both baseline and monthly data.
         let stations;
         if (this.props.dataset === 'anomaly') {
-            const monthlyByStationDbId = _.groupBy(this.props.monthly, 'station_db_id');
+            const monthlyByStationDbId = _.groupBy(
+              this.props.monthly, 'station_db_id'
+            );
             stations = [];
             this.props.baseline.forEach(baselineStation => {
-                const monthlyStation = monthlyByStationDbId[baselineStation.station_db_id];
+                const monthlyStation = monthlyByStationDbId[
+                  baselineStation.station_db_id
+                ];
                 if (monthlyStation) {
-                    const anomaly = monthlyStation[0].statistic - baselineStation.datum;
-                    const departure = 100 * ((monthlyStation[0].statistic / baselineStation.datum) - 1);
+                    const anomaly =
+                      monthlyStation[0].statistic - baselineStation.datum;
+                    const departure = 100 * (
+                      (monthlyStation[0].statistic / baselineStation.datum) - 1
+                    );
                     stations.push({
-                        ...pick(baselineStation, 'station_name lat lon elevation station_db_id network_variable_name'),
+                        ...pick(
+                          baselineStation,
+                          'station_name lat lon elevation station_db_id ' +
+                          'network_variable_name'
+                        ),
                         anomaly,
                         departure,
                     });
