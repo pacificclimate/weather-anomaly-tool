7c8a5a60d17e4908058e10a5f2a909dcac06957a
rod-glover <rod.glover.ca@gmail.com>
2021-11-11 14:09:51 -0800

    Refactor Tool with hooks

--- src/components/Tool/Tool.js
+++ src/components/Tool/Tool.js
@@ -1,8 +1,7 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import moment from 'moment';
 
-import { bindFunctions, pick } from '../utils';
 import DatasetSelector from '../DatasetSelector'
 import VariableSelector from '../VariableSelector'
 import YearSelector from '../YearSelector';
@@ -12,11 +11,12 @@ import DataViewer from '../DataViewer';
 
 import 'react-input-range/lib/css/index.css';
 import './Tool.css';
-import { getLastDateWithDataBefore } from '../../data-services/weather-anomaly-data-service';
+import { getLastDateWithDataBefore }
+    from '../../data-services/weather-anomaly-data-service';
 
 
 // Note: We use package `moment` for date arithmetic. It is excellent but it
-// *mutates* its objects. We are using React PureComponents in this application,
+// *mutates* its objects. We are using functional components,
 // which require values whose identity changes when their value is changed,
 // i.e., a new object. Therefore every change to a `moment` date object should
 // be preceded by `.clone()`; for example, `y = x.clone().subtract(1, 'month')`
@@ -28,150 +28,130 @@ import { getLastDateWithDataBefore } from '../../data-services/weather-anomaly-d
 // This allows for cron jobs that run in first half of month.
 // Subtract fewer/more days if cron jobs run earlier/later in month.
 // But it is not guaranteed that there is data for this date; that can only be
-// determined by consulting the backend. That's done in `componentDidMount()`.
+// determined by consulting the backend.
 const latestPossibleDataDate = moment().subtract(15, 'days');
 
-// TODO: Make these props?
-const monthIncrDecrBy = [1, 3, 6];
-const yearIncrDecrBy = [1, 2, 3, 4, 5, 10];
+export default function Tool({
+    monthIncrDecrBy = [1, 3, 6],
+    yearIncrDecrBy = [1, 2, 3, 4, 5, 10],
+}) {
+    const [dataset, setDataset] = useState('anomaly');
+    const [variable, setVariable] = useState('precip');
+    const [date, setDate] = useState(latestPossibleDataDate);
+    const [dataLoading, setDataLoading] = useState(true);
 
+    // Determine latest date with data, and set date to it.
+    useEffect(() => {
+        setDataLoading(true);
+        getLastDateWithDataBefore(variable, date)
+        .then(date => {
+            setDate(date);
+            setDataLoading(false);
+        });
+    }, [])
 
-export default class Tool extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataset: 'anomaly',
-            variable: 'precip',
-            date: latestPossibleDataDate,
-            dataLoading: true,
-        };
-        bindFunctions(
-          this,
-          'handleChangeVariable handleChangeDataset handleChangeMonth ' +
-          'handleChangeYear handleIncrementYear handleIncrementMonth ' +
-          'handleDataIsLoading handleDataIsNotLoading'
-        );
+    const handleChangeMonth = (month) => {
+        setDate((date) => date.clone().month(month));
+    };
+
+    const handleChangeYear = (year) => {
+        setDate((date) => date.clone().year(year));
+    };
+
+    const handleIncrementYear = (by) => {
+        setDate((date) => date.clone().add(by, 'year'));
+    };
+
+    const handleIncrementMonth = (by) =>  {
+        setDate((date) => date.clone().add(by, 'month'));
+    };
+
+    const handleDataIsLoading = () => {
+        setDataLoading(true);
     }
 
-    componentDidMount() {
-        // Set the year and month to the last date before present with data.
-        // This happens only once, when the component mounts.
-        this.setState({ dataLoading: true });
-        getLastDateWithDataBefore(this.state.variable, this.state.date)
-            .then(date => {
-                this.setState({ date, dataLoading: false });
-            });
-    }
+    const handleDataIsNotLoading = () => {
+        setDataLoading(false);
+    };
 
-    handleChangeVariable(variable) {
-        this.setState({variable});
-    }
+    const isBaselineDataset = dataset === 'baseline';
 
-    handleChangeDataset(dataset) {
-        this.setState({dataset});
-    }
-
-    handleChangeMonth(month) {
-        this.setState(({ date }) => ({ date: date.clone().month(month) }));
-    }
-
-    handleChangeYear(year) {
-        this.setState(({ date }) => ({ date: date.clone().year(year) }));
-    }
-
-    handleIncrementYear(by) {
-        this.setState(({ date }) => ({ date: date.clone().add(by, 'year') }));
-    }
-
-    handleIncrementMonth(by) {
-        this.setState(({ date }) => ({ date: date.clone().add(by, 'month') }));
-    }
-
-    handleDataIsLoading() {
-        this.setState({ dataLoading: true });
-    }
-
-    handleDataIsNotLoading() {
-        this.setState({ dataLoading: false });
-    }
-
-    render() {
-        const isBaselineDataset = this.state.dataset === 'baseline';
-        return (
-          <React.Fragment>
-            <Row className="Tool">
-                <Col lg={3} className="selectors">
-                    <Row>Display</Row>
-                    <Row>
-                        <Col lg={2}/>
-                        <Col lg={4}>
-                            <VariableSelector
-                                vertical
-                                disabled={this.state.dataLoading}
-                                value={this.state.variable}
-                                onChange={this.handleChangeVariable}
-                            />
-                        </Col>
-                        <Col lg={4}>
-                            <DatasetSelector
-                                vertical
-                                value={this.state.dataset}
-                                onChange={this.handleChangeDataset}
-                            />
-                        </Col>
-                        <Col lg={2}/>
-                    </Row>
-                    <Row>for</Row>
-                    <Row>
-                        <Col lg={8}>
-                            <MonthSelector
-                                disabled={this.state.dataLoading}
-                                value={this.state.date.month()}
-                                onChange={this.handleChangeMonth}
-                            />
-                        </Col>
-                        <Col lg={4}>
-                            <IncrementDecrement
-                                disabled={this.state.dataLoading}
-                                id="month-increment"
-                                bsSize="xsmall"
-                                by={monthIncrDecrBy}
-                                onIncrement={this.handleIncrementMonth}
-                            />
-                        </Col>
-                    </Row>
-                    {!isBaselineDataset &&
-                    <Row>
-                        <Col lg={8}>
-                            <YearSelector
-                                disabled={this.state.dataLoading}
-                                start={1970}
-                                end={latestPossibleDataDate.year()}
-                                value={this.state.date.year()}
-                                onChange={this.handleChangeYear}
-                            />
-                        </Col>
-                        <Col lg={4}>
-                            <IncrementDecrement
-                                disabled={this.state.dataLoading}
-                                id="year-increment"
-                                bsSize="xsmall"
-                                by={yearIncrDecrBy}
-                                onIncrement={this.handleIncrementYear}
-                            />
-                        </Col>
-                    </Row>}
-                </Col>
-                <Col  lg={9}>
-                    <DataViewer
-                        {...pick(this.state, 'dataset variable date')}
-                        onDataWillLoad={this.handleDataIsLoading}
-                        onDataDidLoad={this.handleDataIsNotLoading}
-                        onDataDidCatch={this.handleDataIsNotLoading}
-                    />
-                </Col>
-            </Row>
-          </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        <Row className="Tool">
+            <Col lg={3} className="selectors">
+                <Row>Display</Row>
+                <Row>
+                    <Col lg={2}/>
+                    <Col lg={4}>
+                        <VariableSelector
+                            vertical
+                            disabled={dataLoading}
+                            value={variable}
+                            onChange={setVariable}
+                        />
+                    </Col>
+                    <Col lg={4}>
+                        <DatasetSelector
+                            vertical
+                            value={dataset}
+                            onChange={setDataset}
+                        />
+                    </Col>
+                    <Col lg={2}/>
+                </Row>
+                <Row>for</Row>
+                <Row>
+                    <Col lg={8}>
+                        <MonthSelector
+                            disabled={dataLoading}
+                            value={date.month()}
+                            onChange={handleChangeMonth}
+                        />
+                    </Col>
+                    <Col lg={4}>
+                        <IncrementDecrement
+                            disabled={dataLoading}
+                            id="month-increment"
+                            bsSize="xsmall"
+                            by={monthIncrDecrBy}
+                            onIncrement={handleIncrementMonth}
+                        />
+                    </Col>
+                </Row>
+                {!isBaselineDataset &&
+                <Row>
+                    <Col lg={8}>
+                        <YearSelector
+                            disabled={dataLoading}
+                            start={1970}
+                            end={latestPossibleDataDate.year()}
+                            value={date.year()}
+                            onChange={handleChangeYear}
+                        />
+                    </Col>
+                    <Col lg={4}>
+                        <IncrementDecrement
+                            disabled={dataLoading}
+                            id="year-increment"
+                            bsSize="xsmall"
+                            by={yearIncrDecrBy}
+                            onIncrement={handleIncrementYear}
+                        />
+                    </Col>
+                </Row>}
+            </Col>
+            <Col  lg={9}>
+                <DataViewer
+                    dataset={dataset}
+                    variable={variable}
+                    date={date}
+                    onDataWillLoad={handleDataIsLoading}
+                    onDataDidLoad={handleDataIsNotLoading}
+                    onDataDidCatch={handleDataIsNotLoading}
+                />
+            </Col>
+        </Row>
+      </React.Fragment>
+    );
 }
