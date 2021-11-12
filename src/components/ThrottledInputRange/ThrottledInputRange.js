18a3eeea0d42a5e80c9b463c0347a72cddc6ec19
rod-glover <rod.glover.ca@gmail.com>
2021-11-11 20:28:29 -0800

    Add component ThrottledInputRange

---
+++ src/components/ThrottledInputRange/ThrottledInputRange.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import InputRange from 'react-input-range';
+
+import './ThrottledInputRange.css';
+
+function ThrottledInputRange({ value, onChange, ...rest }) {
+    const [intermediateValue, setIntermediateValue] = useState(value);
+
+    useEffect(() => {
+        setIntermediateValue(value);
+    }, [value]);
+
+    return (
+        <InputRange
+            value={intermediateValue}
+            onChange={setIntermediateValue}
+            onChangeComplete={onChange}
+            {...rest}
+        />
+    );
+}
+
+ThrottledInputRange.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired,
+};
+
+export default ThrottledInputRange;
