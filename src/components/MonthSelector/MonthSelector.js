f244377b61ff65f7da39134a675ff86026b16756
rod-glover <rod.glover.ca@gmail.com>
2021-11-11 20:28:50 -0800

    Refactor MonthSelector, YearSelector

--- src/components/MonthSelector/MonthSelector.js
+++ src/components/MonthSelector/MonthSelector.js
@@ -1,70 +1,19 @@
-// TODO: YearSelector and MonthSelector are nearly identical. Use a HOC for them both.
+import React from 'react';
 
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-
-import InputRange from 'react-input-range';
-
-import { bindFunctions, pick } from '../utils';
-
-import './MonthSelector.css';
+import ThrottledInputRange from '../ThrottledInputRange';
 
 const monthNames = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' ');
+const formatLabel = value => monthNames[value];
 
-class MonthSelector extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value
-        };
-        bindFunctions(this, 'handleChange');
-    }
-
-    handleChange(value) {
-        this.setState({value})
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.value !== this.state.value) {
-            this.setState({value: nextProps.value});
-        }
-    }
-
-    formatLabel = value => monthNames[value - this.props.start];
-
-    render() {
-        return (
-            <InputRange
-                {...pick(this.props, 'className disabled')}
-                minValue={this.props.start}
-                maxValue={this.props.end}
-                formatLabel={this.formatLabel}
-                value={this.state.value}
-                onChange={this.handleChange}
-                onChangeComplete={this.props.onChange}
-            />
-        )
-    }
+function MonthSelector(props) {
+    return (
+        <ThrottledInputRange
+            minValue={0}
+            maxValue={11}
+            formatLabel={formatLabel}
+            {...props}
+        />
+    );
 }
-
-MonthSelector.propTypes = {
-    disabled: PropTypes.bool,
-    // Is control disabled
-    start: PropTypes.number,
-    // Start month
-    end: PropTypes.number,
-    // End month
-    value: PropTypes.number,
-    // Current value (month)
-    onChange: PropTypes.func.isRequired,
-    // Called with value when dragging complete (no intermediate values)
-};
-
-MonthSelector.defaultProps = {
-    disabled: false,
-    start: 0,
-    end: 11,
-};
-
 
 export default MonthSelector;
