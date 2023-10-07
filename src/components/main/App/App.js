b83de653293f0f32a1bbb7167492157190e7cc66
rod-glover <rod.glover.ca@gmail.com>
2023-10-07 12:28:36 -0700

    Add context provider (initial version)

--- src/components/main/App/App.js
+++ src/components/main/App/App.js
@@ -4,17 +4,44 @@ import { Container } from 'react-bootstrap';
 import logger from '../../../logger';
 import Header from '../Header';
 import Tool from '../Tool'
+import ConfigContext, { useFetchConfigContext } from '../ConfigContext';
 
 import './App.css';
 
 logger.configure({ active: true });
 
 function App() {
+  // must be invoked before any other items dependent on context.
+  const [config, configErrorMessage] = useFetchConfigContext({
+    defaultConfig: {
+      // TODO
+    },
+    requiredConfigKeys: [
+      // TODO
+    ],
+  });
+
+  if (configErrorMessage !== null) {
+    // TODO: Improve presentation
+    return (
+      <div>
+        <div>{configErrorMessage}</div>
+      </div>
+    )
+  }
+
+  if (config === null) {
+    // TODO: Replace with spinner
+    return <div>Loading configuration...</div>
+  }
+
   return (
-    <Container fluid className="App">
-      <Header/>
-      <Tool/>
-    </Container>
+    <ConfigContext.Provider value={config}>
+      <Container fluid className="App">
+        <Header/>
+        <Tool/>
+      </Container>
+    </ConfigContext.Provider>
   );
 }
 
