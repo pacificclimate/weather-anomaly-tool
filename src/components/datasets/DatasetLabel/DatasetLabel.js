a9b85c282d00fa84ac1a9875f54712a313d5b4d4
rod-glover <rod.glover.ca@gmail.com>
2023-10-10 09:49:16 -0700

    Add component DatasetLabel

---
+++ src/components/datasets/DatasetLabel/DatasetLabel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useConfigContext } from '../../main/ConfigContext';
+
+export default function DatasetLabel({ dataset }) {
+  const config = useConfigContext();
+  return (
+    <span dangerouslySetInnerHTML={{
+      __html: config?.datasets?.[dataset]?.label ?? dataset
+    }}/>
+  );
+}
