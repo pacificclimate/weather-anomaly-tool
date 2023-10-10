47f4dfad5270d5fcd0fbf5d1a30406ef7186cfcc
rod-glover <rod.glover.ca@gmail.com>
2023-10-09 17:36:20 -0700

    Add VariableLabel

--- src/components/misc/VariableLabel/VariableLabel.js
+++ src/components/misc/VariableLabel/VariableLabel.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { variableLabels } from '../../../utils/variables';
+import { useConfigContext } from '../../main/ConfigContext';
 
 export default function VariableLabel({ variable }) {
-  return variableLabels[variable] || `${variable}`;
+  const config = useConfigContext();
+  return <span dangerouslySetInnerHTML={{
+    __html: config?.variables?.[variable]?.label || `${variable}`
+  }}/> ;
 }
