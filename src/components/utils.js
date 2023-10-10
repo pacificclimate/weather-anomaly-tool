46085c18b2fe8ec144e0764dce28506e8f9e48c4
rod-glover <rod.glover.ca@gmail.com>
2023-10-09 17:34:32 -0700

    Clean up utils; add function alternate

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,19 +1,25 @@
-import _ from 'lodash';
-
-
-export function bindFunctions(obj, functionNames) {
-  if (typeof functionNames === 'string') {
-    functionNames = functionNames.split(/\s+/);
-  }
-  functionNames.forEach((name) => {
-    obj[name] = obj[name].bind(obj);
-  });
-}
+import _pick from 'lodash/pick';
+import curry from 'lodash/fp/curry';
+import flatten from 'lodash/fp/flatten';
+import flow from 'lodash/fp/flow';
+import map from 'lodash/fp/map';
 
 
 export function pick(obj, names) {
   if (typeof names === 'string') {
     names = names.split(/\s+/);
   }
-  return _.pick(obj, names);
+  return _pick(obj, names);
 }
+
+
+export const mapWithKey = map.convert({ 'cap': false });
+
+// Return a list alternating items from a list with a separator object.
+// This is in a certain way analogous to `join` for strings.
+export const alternate = curry((sep, a) =>
+  flow(
+    mapWithKey((item, i) => i === 0 ? [item] : [sep, item]),
+    flatten,
+  )(a));
+
