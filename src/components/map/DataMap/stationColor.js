72e556a5f0e154bf9daa8136df80382331d62e26
rod-glover <rod.glover.ca@gmail.com>
2023-10-09 17:39:34 -0700

    Make stationColor more robust

--- src/components/map/DataMap/stationColor.js
+++ src/components/map/DataMap/stationColor.js
@@ -13,15 +13,15 @@ function dataValueName(variable, dataset){
 }
 
 function valueToColourScaleItem(value, colourScale) {
-  return _.find(colourScale, item => value < item.threshold);
+  return colourScale && _.find(colourScale, item => value < item.threshold);
 }
 
 
 export function stationColor(variable, dataset, station, colourScales){
   // Return a color code for the marker for station.
   const value = station[dataValueName(variable, dataset)];
-  const colourScale = colourScales[variable][dataset];
+  const colourScale = colourScales?.[variable]?.[dataset];
   return {
-    fillColor: valueToColourScaleItem(value, colourScale).color,
+    fillColor: valueToColourScaleItem(value, colourScale)?.color,
   };
 }
