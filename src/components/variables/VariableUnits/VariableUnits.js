3d603a3e46ae7ad0fd53f93243528504c59f4791
rod-glover <rod.glover.ca@gmail.com>
2023-11-20 16:34:34 -0800

    Use config from store in remaining components

--- src/components/variables/VariableUnits/VariableUnits.js
+++ src/components/variables/VariableUnits/VariableUnits.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useConfigContext } from '../../main/ConfigContext';
+import { useStore } from '../../../state-store';
 
 export default function VariableUnits({ variable }) {
-  const config = useConfigContext();
+  const config = useStore(state => state.config);
   return (
     <span dangerouslySetInnerHTML={{
       __html: config?.variables?.[variable]?.units ?? 'bar'
