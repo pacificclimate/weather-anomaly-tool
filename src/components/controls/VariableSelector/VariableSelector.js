227e100fc21d048689f5a44a716c295d58c34b66
rod-glover <rod.glover.ca@gmail.com>
2023-11-20 16:23:01 -0800

    Convert VariableSelector to use store

--- src/components/controls/VariableSelector/VariableSelector.js
+++ src/components/controls/VariableSelector/VariableSelector.js
@@ -3,19 +3,34 @@ import PropTypes from 'prop-types';
 import { Tooltip } from 'react-bootstrap';
 import keys from 'lodash/fp/keys';
 
-import { useConfigContext } from '../../main/ConfigContext';
 import RadioButtonSelector from '../RadioButtonSelector';
 import VariableLabel from '../../variables/VariableLabel';
+import { useStore } from '../../../state-store';
 
 
 function VariableSelector(props) {
-  const config = useConfigContext();
+  const config = useStore(state => state.config);
+
+  // TODO: Memoize variableOptions
   const variableKeys = keys(config?.variables);
   const variableOptions = variableKeys.map(
     value => ({ value, label: <VariableLabel variable={value}/> })
   );
+
+  const variable = useStore(state => state.variable);
+  const setVariable = useStore(state => state.setVariable);
+  const isDataLoading = useStore(state => state.isDataLoading);
+
   return (
-    <RadioButtonSelector options={variableOptions} name="variable" {...props} />
+    <RadioButtonSelector
+      options={variableOptions}
+      name="variable"
+      disabled={isDataLoading()}
+      value={variable}
+      onChange={setVariable}
+      styling={config.ui.variableSelector.styling}
+      {...props}
+    />
   );
 }
 
