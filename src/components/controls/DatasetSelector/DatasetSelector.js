575edbd8a69d6fbf334a2aa3a3bc89f7d17b44f6
rod-glover <rod.glover.ca@gmail.com>
2023-11-20 16:22:38 -0800

    Convert DatasetSelector to use store

--- src/components/controls/DatasetSelector/DatasetSelector.js
+++ src/components/controls/DatasetSelector/DatasetSelector.js
@@ -6,19 +6,32 @@ import map from 'lodash/fp/map';
 
 import DatasetLabel from '../../datasets/DatasetLabel';
 import RadioButtonSelector from '../RadioButtonSelector';
-import { useConfigContext } from '../../main/ConfigContext';
+import { useStore } from '../../../state-store';
 
 
 function DatasetSelector(props) {
-  const config = useConfigContext();
+  const config = useStore(state => state.config);
+
+  // TODO: Memoize
   const options = flow(
     keys,
     map(dataset =>
         ({ value: dataset, label: <DatasetLabel dataset={dataset}/> })
     ),
   )(config.datasets);
+
+  const dataset = useStore(state => state.dataset);
+  const setDataset = useStore(state => state.setDataset);
+
   return (
-    <RadioButtonSelector name="dataset" options={options} {...props}/>
+    <RadioButtonSelector
+      name="dataset"
+      options={options}
+      value={dataset}
+      onChange={setDataset}
+      styling={config.ui.datasetSelector.styling}
+      {...props}
+    />
   );
 }
 
