95bcd07bbc99cb3c1c6eb9b163d5171b237397c5
rod-glover <rod.glover.ca@gmail.com>
2024-06-27 16:20:30 -0700

    Clean up

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -1,10 +1,6 @@
 import { Stack } from "react-bootstrap";
-import HelpItem from "@/components/help/HelpItem";
-import Markdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
 import React from "react";
 import useConfigContext from "@/state/context-hooks/use-config-context";
-import usePublicFile from "@/state/query-hooks/use-public-file";
 import HelpItemFromFile from "@/components/help/HelpItemFromFile";
 
 export default function Help() {
